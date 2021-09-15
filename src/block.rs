98720faab6284742754fbc9e9c45a2a8ef13de6a
Paht J <paht.juangphanich@gmail.com>
2021-09-14 23:22:41 -0400

    added comments

--- src/block.rs
+++ src/block.rs
@@ -1,12 +1,20 @@
 use std::fmt::{ self, Debug, Formatter };
 use super::{BlockHash,u128_bytes, u32_bytes, u64_bytes, Hashable};
 
+/// Strategy 
+/// 1. Generate a nonce
+/// 2. Hash bytes (heavy step)
+/// 3. Check hash against difficulty 
+/// 4. Add Block to chain
+/// 5. Submit to peers (skip this step)
+
 pub struct Block {
+    /// nonce - if you generate a hash and doesn't fit the difficulty so we change bytes that we are hashing but this is bad. So we introduce nonce. Nonce is arbitrary data and change it at will and eventually when hashed, that will generate a hash that matches the difficulty. 
     pub index: u32,
     pub timestamp: u128,
     pub hash: BlockHash,
     pub prev_block_hash: BlockHash,
-    pub nonce: u64,
+    pub nonce: u64, 
     pub payload: String,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use blockchainlib::{Block,Hashable};
 
+// Note about Mining: You cannot derive the data from the hash but you can get the hash from whatever data
+
+//Difficulty - the unsigned 128 bit integer value that the most significant 16 bytes of hash must be less than before it's considered valid. Number of bits/bytes at beginning of the hash that must be 0 
+
+// Little vs. Big Endian - The order of bytes stored in memory
+// Example 42u32
+// Big Endian 00 00 00 2a
+// Little Endian 2a 00 00 00
 fn main () {
     let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block!".to_owned());
 
