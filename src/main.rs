845f2ba78f9fc7fedf7331820ee71791bf1fe257
Paht J <paht.juangphanich@gmail.com>
2021-09-18 23:00:22 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,21 @@ fn main () {
     println!("Verify: {}", &blockchain.verify());
 }
 
-//  Block verification
-//  1. Actual index == stored index value 
-//  2. Block's hash fits stored difficulty value (it's insecure to simply trust the difficulty)
-//  3. Time is always increasing 
-//  4. Actual previous block's hash == stored prev_block_hash value 
+// Block verification
+// 1. Actual index == stored index value 
+// 2. Block's hash fits stored difficulty value (it's insecure to simply trust the difficulty)
+// 3. Time is always increasing 
+// 4. Actual previous block's hash == stored prev_block_hash value 
+// 5. Blockchain is a distributed ledger, everyone has a history of where they got their coins
 
+// Block Transactions
+// Alice has 50 coins
+// Bob has 7 coins
+// Alice sends bob coins
+// * A Blockchain isn't a spreadsheet!
+// Needs:
+//  - Protect against overspending
+//  - No double spending
+//  - Impersonation (who owns the money)
+//  - More but need to look into https://en.bitcoin.it/wiki/Protocol_rules#.22tx.22_messages 
 
